2c54ea48d178feab0086c99e1637af1867d8dcfa
Paul Horn <knutwalker@gmail.com>
2016-08-23 01:20:17 +0200

    'FromMap'

---
+++ src/main/scala/FromMap.scala
@@ -0,0 +1,92 @@
+
+import validation.Result
+import validation.Result.fromOption
+
+import shapeless._
+import shapeless.labelled._
+
+
+sealed trait Error
+
+case class Missing(key: String) extends Error
+case class WrongType(key: String, expected: String) extends Error
+case class Mistyped[A](key: String, value: Any)(implicit val expexted: Typeable[A]) extends Error
+
+
+trait FromMap[R <: HList] extends ((Map[String, Any], Option[String]) => Result[Error, R]) {
+  def apply(m: Map[String, Any], parent: Option[String]): Result[Error, R]
+}
+
+trait LowPriorityFromMap0 {
+  implicit def hconsFromMap0[K <: Symbol, V, T <: HList](
+    implicit
+    K: Witness.Aux[K],
+    V: Typeable[V],
+    T: Lazy[FromMap[T]]
+  ): FromMap[FieldType[K, V] :: T] = (m, p) => {
+    val name = K.value.name
+    val fullName = p.fold(name)(_ + s".$name")
+    val value = for {
+      v <- fromOption(m.get(name), Missing(fullName))
+      x <- fromOption(V.cast(v), Mistyped[V](fullName, v))
+    } yield x
+    val tail = T.value(m, p)
+    (value and tail) ((v, t) => field[K](v) :: t)
+  }
+}
+
+trait LowPriorityFromMap1 extends LowPriorityFromMap0 {
+  implicit def hconsFromMap1[K <: Symbol, V, R <: HList, T <: HList](
+    implicit
+    K: Witness.Aux[K],
+    V: LabelledGeneric.Aux[V, R],
+    R: Lazy[FromMap[R]],
+    T: Lazy[FromMap[T]]
+  ): FromMap[FieldType[K, V] :: T] = (m, p) => {
+    val name = K.value.name
+    val fullName = p.fold(name)(_ + s".$name")
+    val value = for {
+      k <- fromOption(m.get(name), Missing(fullName))
+      v <- fromOption(Typeable[Map[String, Any]].cast(k), WrongType(fullName, "nested map"))
+      x <- R.value(v, Some(fullName))
+    } yield x
+    val tail = T.value(m, p)
+    (value and tail) ((r, t) => field[K](V.from(r)) :: t)
+  }
+}
+
+object FromMap extends LowPriorityFromMap1 {
+  private[this] val some_hnil                  = Result.valid(HNil)
+  implicit      val hnilFromMap: FromMap[HNil] = (m, p) => some_hnil
+
+  implicit def hconsFromMap2[K <: Symbol, V, R <: HList, T <: HList](
+    implicit
+    K: Witness.Aux[K],
+    V: LabelledGeneric.Aux[V, R],
+    R: Lazy[FromMap[R]],
+    T: Lazy[FromMap[T]]
+  ): FromMap[FieldType[K, List[V]] :: T] = (m, p) => {
+    val name = K.value.name
+    val fullName = p.fold(name)(_ + s".$name")
+
+    val value = for {
+      k <- fromOption(m.get(name), Missing(fullName))
+      xs <- fromOption(Typeable[List[Map[String, Any]]].cast(k), WrongType(fullName, "nested list of maps"))
+      x <- Result.traverse(xs.zipWithIndex) {case (v, i) => R.value(v, Some(s"$fullName.$i"))}
+    } yield x
+
+    val tail = T.value(m, p)
+    (value and tail) ((r, t) => field[K](r.map(V.from)) :: t)
+  }
+
+  class FromMapper[A] {
+    def apply[R <: HList](m: Map[String, Any])(
+      implicit
+      A: LabelledGeneric.Aux[A, R],
+      R: FromMap[R]
+    ): Result[Error, A] =
+      R(m, None).map(A.from)
+  }
+
+  def to[A](implicit A: LabelledGeneric[A]) = new FromMapper[A]
+}
---
+++ src/test/scala/FromMapSpec.scala
@@ -0,0 +1,130 @@
+import validation.Result.{ Invalid, Invalids, Valid }
+
+import org.scalatest.{ FlatSpec, Inside, Matchers }
+
+
+object FromMapSpec {
+  case class Address(street: String, zip: Int)
+  case class Person(name: String, addresses: List[Address])
+}
+
+final class FromMapSpec extends FlatSpec with Inside with Matchers {
+  import FromMapSpec._
+  val mapper = FromMap.to[Person]
+
+  behavior of "FromMap"
+
+  it should "build a person" in {
+
+    val mp = Map[String, Any](
+      "name" -> "Tom",
+      "addresses" -> List(Map[String, Any]("street" -> "Jefferson st", "zip" -> 10000))
+    )
+
+    inside(mapper(mp)) {
+      case Valid(person) =>
+        person.name shouldBe "Tom"
+        person.addresses should have size 1
+        person.addresses.head.street shouldBe "Jefferson st"
+        person.addresses.head.zip shouldBe 10000
+    }
+  }
+
+  it should "only accept List, not Vector for nested lists of maps" in {
+
+    val mp = Map[String, Any](
+      "name" -> "Tom",
+      "addresses" -> Vector(Map[String, Any]("street" -> "Jefferson st", "zip" -> 10000))
+    )
+
+    inside(mapper(mp)) {
+      case Invalid(error) =>
+        error shouldBe WrongType("addresses", "nested list of maps")
+    }
+  }
+
+  it should "report a missing name entry" in {
+    val mp = Map[String, Any](
+      "names" -> "Tom",
+      "addresses" -> List(Map[String, Any]("street" -> "Jefferson st", "zip" -> 10000))
+    )
+
+    inside(mapper(mp)) {
+      case Invalid(error) =>
+        error shouldBe Missing("name")
+    }
+  }
+
+  it should "report a missing addresses entry" in {
+
+    val mp = Map[String, Any](
+      "name" -> "Tom",
+      "address" -> Map("street" -> "Jefferson st", "zip" -> 10000)
+    )
+
+    inside(mapper(mp)) {
+      case Invalid(error) =>
+        error shouldBe Missing("addresses")
+    }
+  }
+
+  it should "not accept a map when a list is required" in {
+
+    val mp = Map[String, Any](
+      "name" -> "Tom",
+      "addresses" -> Map("streets" -> "Jefferson st", "zip" -> "10000")
+    )
+
+    inside(mapper(mp)) {
+      case Invalid(error) =>
+        error shouldBe WrongType("addresses", "nested list of maps")
+    }
+  }
+
+  it should "report multiple nested errors" in {
+    val mp = Map(
+      "name" -> "Tom",
+      "addresses" -> List(Map("streets" -> "Jefferson st", "zip" -> "10000"))
+    )
+
+    inside(mapper(mp)) {
+      case Invalids(es) =>
+        val errors = es.toVector
+        errors should have size 2
+        errors(0) shouldBe Missing("addresses.0.street")
+        errors(1) shouldBe Mistyped[Int]("addresses.0.zip", "10000")
+    }
+  }
+
+  it should "report type mismatch if some other elements are correct" in {
+    val mp = Map(
+      "name" -> "Tom",
+      "addresses" -> List(Map[String, Any]("street" -> "Jefferson st", "zip" -> 10000), "foo")
+    )
+
+    inside(mapper(mp)) {
+      case Invalid(error) =>
+        error shouldBe WrongType("addresses", "nested list of maps")
+    }
+  }
+
+  it should "accumulate all errors" in {
+    val mp = Map(
+      "name" -> 't',
+      "addresses" -> List(
+        Map[String, Any]("streets" -> "Jefferson st", "zip" -> "10000"),
+        Map[String, Any]("street" -> 'b', "sip" -> 10000))
+    )
+
+    inside(mapper(mp)) {
+      case Invalids(es) =>
+        val errors = es.toVector
+        errors should have size 5
+        errors(0) shouldBe Mistyped[String]("name", 't')
+        errors(1) shouldBe Missing("addresses.0.street")
+        errors(2) shouldBe Mistyped[Int]("addresses.0.zip", "10000")
+        errors(3) shouldBe Mistyped[String]("addresses.1.street", 'b')
+        errors(4) shouldBe Missing("addresses.1.zip")
+    }
+  }
+}
